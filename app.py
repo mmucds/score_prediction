75ac75d7603317913aca4732d58207ac678adf32
Masbah <mmucds@gmail.com>
2023-06-20 15:01:58 +0100

    save

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = application
 # Route for a home page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 
 @app.route('/predict', methods=['GET', 'POST'])
